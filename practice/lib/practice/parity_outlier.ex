80e2a7260d86418e3ef5a1edf467385c7ea6bfbc
Thomas McLean <tmclean@covermymeds.com>
2022-06-01 08:54:32 -0400

    made some changes based on Jonah's feedback

--- practice/lib/practice/parity_outlier.ex
+++ practice/lib/practice/parity_outlier.ex
@@ -1,26 +1,23 @@
-defmodule Parity_outlier do
-
+defmodule ParityOutlier do
   require Integer
 
   def find_outlier(list) do
-    value = eval_even_or_odd(list)
+    num_of_even_integers = eval_num_of_evens(list)
 
     list
-    |> parse_even_or_odd(value)
+    |> parse_even_or_odd(num_of_even_integers)
     |> Enum.sum
   end
 
-  def eval_even_or_odd(list) do
-    list
-    |> Enum.map(fn x -> Integer.is_even(x) end)
-    |> Enum.count(fn x -> x == true end)
+  def eval_num_of_evens(list) do
+    Enum.count(list, fn x -> Integer.is_even(x) end)
   end
 
-  def parse_even_or_odd(list, value) when value == 1 do
+  def parse_even_or_odd(list, num_of_even_integers) when num_of_even_integers == 1 do
     Enum.reject(list, fn x -> Integer.is_odd(x) end)
   end
 
-  def parse_even_or_odd(list, _value) do
+  def parse_even_or_odd(list, _num_of_even_integers) do
     Enum.reject(list, fn x -> Integer.is_even(x) end)
   end
 end
--- practice/test/parity_outlier_test.exs
+++ practice/test/parity_outlier_test.exs
@@ -2,10 +2,10 @@ defmodule TestSuite do
   use ExUnit.Case
 
   test "Sample tests" do
-    assert Parity_outlier.find_outlier([1,2,3]) == 2
-    assert Parity_outlier.find_outlier([2,6,8,10,3]) == 3
-    assert Parity_outlier.find_outlier([0,0,3,0,0]) == 3
-    assert Parity_outlier.find_outlier([1,1,0,1,1]) == 0
-    assert Parity_outlier.find_outlier([0,1,2]) == 1
+    assert ParityOutlier.find_outlier([1,2,3]) == 2
+    assert ParityOutlier.find_outlier([2,6,8,10,3]) == 3
+    assert ParityOutlier.find_outlier([0,0,3,0,0]) == 3
+    assert ParityOutlier.find_outlier([1,1,0,1,1]) == 0
+    assert ParityOutlier.find_outlier([0,1,2]) == 1
   end
 end
