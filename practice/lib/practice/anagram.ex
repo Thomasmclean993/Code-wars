34c5693a11374338fb1e217468d095ac645259c2
Thomas McLean <tmclean@covermymeds.com>
2022-06-11 16:16:28 -0400

    WIP, Last test case red

--- practice/lib/practice/anagram.ex
+++ practice/lib/practice/anagram.ex
@@ -1,21 +1,28 @@
 defmodule Anagram do
   def anagram?(a, b) do
-    list_of_a = remove_dupes(a)
-    list_of_b = remove_dupes(b)
+    converted_a = lowercase(a)
+    converted_b = lowercase(b)
 
-    combined_list = list_of_a ++ list_of_b |> Enum.uniq
-    IO.inspect(combined_list)
-    case combined_list do
-      list_of_a -> true
-      list_of_b -> true
-      _ -> false
-    end
+    with :ok <- parse_anagram(converted_b, converted_a),
+        :ok <- parse_anagram(converted_a, converted_b) do
+        true
+        else
+          _err -> false
+        end
   end
 
-  def remove_dupes(string) do
-    string
-    |> String.downcase
-    |> String.split("", trim: true)
-    |> Enum.uniq
+  def parse_anagram(converted_string1, converted_string2) do
+    String.split(converted_string1, "", trim: true)
+    |> remove_dupes(converted_string2)
+    |> list_contains
   end
+
+  def remove_dupes(list1,list2) do
+  list1
+  |> Enum.reject(fn x -> String.contains?(list2, x) end)
+  end
+
+  def lowercase(value), do: String.downcase(value)
+  def list_contains([]), do: :ok
+  def list_contains(_), do: :error
 end
