f89664f138bc27f7f59f8175095cf52414873995
Thomas McLean <tmclean@covermymeds.com>
2022-06-13 09:16:19 -0400

    Adding testing files to Review

--- practice/test/digital_root_test.exs
+++ practice/test/digital_root_test.exs
@@ -1,6 +1,7 @@
 defmodule TestSuite do
   use ExUnit.Case
 
+
   test "1",do: assert Root.digital_root(1) == 1
   test "16",do: assert Root.digital_root(16) == 7
   test "456",do: assert Root.digital_root(456) == 6
